964748fc61461bff054de7c301e730a3ef41f424
erickmerchant <erickmerchant@gmail.com>
2023-12-28 21:49:36 -0500

    Day 15 - part 2

--- src/day15.rs
+++ src/day15.rs
@@ -1,25 +1,117 @@
+use itertools::Itertools;
 use rayon::prelude::*;
 
 pub fn part1(input: Vec<String>) -> usize {
     let values = input[0]
         .split(',')
-        .map(|s| s.as_bytes().iter().map(|b| *b as u128).collect::<Vec<_>>())
+        .map(|s| s.chars().collect::<Vec<_>>())
         .collect::<Vec<_>>();
 
     let result = values
         .par_iter()
-        .map(|v| v.iter().fold(0, hash))
+        .map(|v| v.iter().fold(0_u128, hash))
         .sum::<u128>();
 
     result as usize
 }
 
-pub fn part2(_input: Vec<String>) -> usize {
-    0
+#[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
+enum OperationType {
+    Remove,
+    Insert(u128),
 }
 
-fn hash(acc: u128, curr: &u128) -> u128 {
-    let mut res = acc + *curr;
+#[derive(Debug, Eq, PartialEq)]
+struct Operation {
+    label: Vec<u128>,
+    hash: u128,
+    operation_type: OperationType,
+}
+
+type Item = (Vec<u128>, u128);
+
+pub fn part2(input: Vec<String>) -> usize {
+    let values = input[0]
+        .split(',')
+        .map(|s| s.chars().collect::<Vec<_>>())
+        .collect::<Vec<_>>();
+    let operations = values
+        .iter()
+        .map(|values| {
+            let mut operation = Operation {
+                label: vec![],
+                hash: 0,
+                operation_type: OperationType::Remove,
+            };
+
+            for value in values {
+                if *value == '-' {
+                    break;
+                } else if *value == '=' {
+                    operation.operation_type = OperationType::Insert(0_u128);
+                } else if let OperationType::Insert(len) = operation.operation_type {
+                    operation.operation_type = OperationType::Insert(
+                        (len * 10)
+                            + value
+                                .to_string()
+                                .parse::<u128>()
+                                .expect("should be parsable as number"),
+                    )
+                } else {
+                    operation.label.push(*value as u128);
+
+                    operation.hash = hash(operation.hash, value);
+                }
+            }
+
+            operation
+        })
+        .collect::<Vec<_>>();
+
+    let mut boxes = vec![Vec::<Item>::new(); 256];
+
+    for operation in operations {
+        match operation.operation_type {
+            OperationType::Insert(len) => {
+                if let Some((i, _)) = boxes[operation.hash as usize]
+                    .iter()
+                    .find_position(|(label, _len)| *label == operation.label)
+                {
+                    boxes[operation.hash as usize][i] = (operation.label, len);
+                } else {
+                    boxes[operation.hash as usize].push((operation.label, len));
+                }
+            }
+            OperationType::Remove => {
+                if let Some((i, _)) = boxes[operation.hash as usize]
+                    .iter()
+                    .find_position(|(label, _len)| *label == operation.label)
+                {
+                    boxes[operation.hash as usize].remove(i);
+                }
+            }
+        }
+    }
+
+    let total = boxes
+        .iter()
+        .enumerate()
+        .map(|(i, items)| {
+            let mut subtotal = 0;
+
+            for (j, (_, len)) in items.iter().enumerate() {
+                subtotal += (i + 1) * (j + 1) * (*len as usize);
+            }
+
+            subtotal
+        })
+        .sum::<usize>();
+
+    total
+}
+
+fn hash(acc: u128, curr: &char) -> u128 {
+    let mut res = acc + (*curr as u128);
 
     res *= 17;
 
@@ -44,7 +136,6 @@ mod tests {
         assert_eq!(part1(get_fixture()), 1320);
     }
 
-    #[ignore]
     #[test]
     fn test_part2() {
         assert_eq!(part2(get_fixture()), 145);
