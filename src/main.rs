19d395ef69d474ddfe80a5c833c3d20a3004f386
erickmerchant <erickmerchant@gmail.com>
2024-01-10 01:05:18 -0500

    Move some modules

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,12 @@
-pub mod day1;
+pub mod day01;
+pub mod day02;
+pub mod day03;
+pub mod day04;
+pub mod day05;
+pub mod day06;
+pub mod day07;
+pub mod day08;
+pub mod day09;
 pub mod day10;
 pub mod day11;
 pub mod day12;
@@ -9,15 +17,7 @@ pub mod day16;
 pub mod day17;
 pub mod day18;
 pub mod day19;
-pub mod day2;
 pub mod day20;
-pub mod day3;
-pub mod day4;
-pub mod day5;
-pub mod day6;
-pub mod day7;
-pub mod day8;
-pub mod day9;
 
 use clap::Parser;
 use core::panic;
@@ -38,24 +38,24 @@ fn main() {
     let handle = stdin.lock();
     let input = handle.lines().flatten().collect();
     let output = match (args.day, args.part) {
-        (1, 1) => day1::part1(input),
-        (1, 2) => day1::part2(input),
-        (2, 1) => day2::part1(input),
-        (2, 2) => day2::part2(input),
-        (3, 1) => day3::part1(input),
-        (3, 2) => day3::part2(input),
-        (4, 1) => day4::part1(input),
-        (4, 2) => day4::part2(input),
-        (5, 1) => day5::part1(input),
-        (5, 2) => day5::part2(input),
-        (6, 1) => day6::part1(input),
-        (6, 2) => day6::part2(input),
-        (7, 1) => day7::part1(input),
-        (7, 2) => day7::part2(input),
-        (8, 1) => day8::part1(input),
-        (8, 2) => day8::part2(input),
-        (9, 1) => day9::part1(input),
-        (9, 2) => day9::part2(input),
+        (1, 1) => day01::part1(input),
+        (1, 2) => day01::part2(input),
+        (2, 1) => day02::part1(input),
+        (2, 2) => day02::part2(input),
+        (3, 1) => day03::part1(input),
+        (3, 2) => day03::part2(input),
+        (4, 1) => day04::part1(input),
+        (4, 2) => day04::part2(input),
+        (5, 1) => day05::part1(input),
+        (5, 2) => day05::part2(input),
+        (6, 1) => day06::part1(input),
+        (6, 2) => day06::part2(input),
+        (7, 1) => day07::part1(input),
+        (7, 2) => day07::part2(input),
+        (8, 1) => day08::part1(input),
+        (8, 2) => day08::part2(input),
+        (9, 1) => day09::part1(input),
+        (9, 2) => day09::part2(input),
         (10, 1) => day10::part1(input),
         (10, 2) => day10::part2(input),
         (11, 1) => day11::part1(input),
